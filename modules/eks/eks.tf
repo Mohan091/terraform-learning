b41589ddf2da568e8ac8b5c7c49985e2b3d0fe66
Mohan Mourya <mohan@gmail.com>
2025-08-01 18:41:19 +0530

    updated eks cluster code

--- modules/eks/eks.tf
+++ modules/eks/eks.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "eks-sg" {
   name   = "${terraform.workspace}-${var.cluster_sg_name}"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = 	"vpc-0f471ac82d8e5a8a8" #module.vpc.vpc_id
 
   ingress {
     from_port   = 80
@@ -16,6 +16,14 @@ resource "aws_security_group" "eks-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow HTTPS from internet"
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true 
+    description = "Allowed from self"
   }
 
   egress {
@@ -57,13 +65,21 @@ module "eks" {
   kubernetes_version                       = "1.31"
   endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
-  vpc_id                                   = "vpc-09e87ebdd907f8fea"  #module.vpc.output.vpc_id
-  subnet_ids                               = ["subnet-04cadadffdbfeaa4b"] #module.vpc.resource.public_subnets.id
+  vpc_id                                   = "vpc-0f471ac82d8e5a8a8"  #module.vpc.output.vpc_id
+  subnet_ids                               = ["subnet-0f764db305c5e677d","subnet-042f9a30c03bb3bbd","subnet-0059910fbcc450a69"] #module.vpc.resource.public_subnets.id
   security_group_id                        = aws_security_group.eks-sg.id
-  addons = {
-    coredns               = {}
-    kube-proxy            = {}
-    amazon_vpc_cni        = {}
-    amazon_ebs_csi_driver = {}
-  }
+  # addons = {
+  #   coredns               = {
+  #     version = "v1.11.3-eksbuild.1"
+  #   }
+  #   kube-proxy            = {
+  #     version = "v1.31.2-eksbuild.3"
+  #   }
+  #   vpc-cni        = {
+  #     version   = "v1.19.0-eksbuild.1"
+  #   }
+  #   aws-ebs-csi-driver = {
+  #     version   = "v2.1.9-eksbuild.1"
+  #   }
+  # }
 }
