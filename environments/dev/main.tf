a0828cd49d01765b35bd3ff6b733ff5e4d6dde9e
Mohan Mourya <mohan@gmail.com>
2025-08-01 18:38:53 +0530

    updated eks cluster code

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,20 +2,20 @@ locals {
   environment = terraform.workspace
 }
 
-module "vpc" {
-  env                = local.environment
-  source             = "../../modules/vpc"
-  vpc_name           = var.vpc_name
-  cidr               = var.cidr_block
-  azs                = var.azs
-  private_subnets    = var.private_subnets
-  public_subnets     = var.public_subnets
-  enable_nat_gateway = var.enable_nat_gateway
-  tags = {
-    Environment = "${terraform.workspace}"
-    Created_By  = "${var.tags.Created_By}"
-  }
-}
+# module "vpc" {
+#   env                = local.environment
+#   source             = "../../modules/vpc"
+#   vpc_name           = var.vpc_name
+#   cidr               = var.cidr_block
+#   azs                = var.azs
+#   private_subnets    = var.private_subnets
+#   public_subnets     = var.public_subnets
+#   enable_nat_gateway = var.enable_nat_gateway
+#   tags = {
+#     Environment = "${terraform.workspace}"
+#     Created_By  = "${var.tags.Created_By}"
+#   }
+# }
 
 module "eks" {
   source               = "../../modules/eks/"
@@ -27,5 +27,5 @@ module "eks" {
   cluster_sg_name      = var.cluster_sg_name
   launch_template_name = "${terraform.workspace}-eks-lt"
   ssm_role             = var.ssm_role
-
+  nodegroup_sg_name    = var.nodegroup_sg_name
 }
